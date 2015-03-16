9ad62e3b54370846042a65653ce054a6571ab2d1
xialanxuan <xialanxuan1015@163.com>
2015-03-16 19:06:50 -0400

    first

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,29 @@ import org.eclipse.jetty.servlet.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.*;
+import com.amazonaws.AmazonClientException;
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.profile.ProfileCredentialsProvider;
+import com.amazonaws.regions.Region;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.Bucket;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ListObjectsRequest;
+import com.amazonaws.services.s3.model.ObjectListing;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectSummary;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 
 public class Main extends HttpServlet {
   @Override
@@ -21,7 +44,37 @@ public class Main extends HttpServlet {
 
   private void showHome(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
+
+      String bucketName = "medidatasiyang";
+      String key = "sandbox_audits_sample.json";
+
+	  AWSCredentials credentials = null;
+           try {
+          credentials = new ProfileCredentialsProvider("default").getCredentials();
+      } catch (Exception e) {
+          throw new AmazonClientException(
+                  "Cannot load the credentials from the credential profiles file. " +
+                  "Please make sure that your credentials file is at the correct " +
+                  "location (/Users/siyang/.aws/credentials), and is in valid format.",
+                  e);
+      }  
+      AmazonS3 s3 = new AmazonS3Client(credentials);
+      Region usWest2 = Region.getRegion(Regions.US_WEST_2);
+      s3.setRegion(usWest2);
+      
+      for (Bucket bucket : s3.listBuckets()) {
+          //resp.getWriter().print(" - " + bucket.getName());
+          if (bucket.getName().equals(bucketName)){
+        	  //resp.getWriter().print("Downloading an object");
+              S3Object object = s3.getObject(new GetObjectRequest(bucketName, key));
+              //resp.getWriter().print("Content-Type: "  + object.getObjectMetadata().getContentType());
+              //resp.getWriter().print("<br>");
+              displayTextInputStream(object.getObjectContent());
+          }
+      }
+      
+	  
+    //resp.getWriter().print("Hello from Java! ?????????");
   }
 
   private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
@@ -68,5 +121,16 @@ public class Main extends HttpServlet {
     context.addServlet(new ServletHolder(new Main()),"/*");
     server.start();
     server.join();
+  }
+  
+  private static void displayTextInputStream(InputStream input) throws IOException {
+      BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+      while (true) {
+          String line = reader.readLine();
+          if (line == null) break;
+
+          System.out.println("    " + line);
+      }
+      System.out.println();
   }
 }
